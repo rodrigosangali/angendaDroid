b4c2e45a56ab39263ed2faaefaa483916b2336d9
Rodrigo <rodrigo.sangali@gmail.com>
2023-10-12 21:11:06 -0300

    first version

---
+++ app/src/main/java/com/sangali/agendadroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sangali.agendadroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Rodrigo Sangali", Toast.LENGTH_LONG).show();
+    }
+}
