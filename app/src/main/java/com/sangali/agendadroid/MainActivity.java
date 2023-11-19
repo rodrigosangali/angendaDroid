7b7aa790a1d8f7459addbca4a6f1d8107b55b101
Rodrigo <rodrigo.sangali@gmail.com>
2023-11-19 20:26:27 -0300

    implementado o list view

--- app/src/main/java/com/sangali/agendadroid/MainActivity.java
+++ app/src/main/java/com/sangali/agendadroid/MainActivity.java
@@ -2,9 +2,17 @@ package com.sangali.agendadroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +20,16 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Rodrigo Sangali", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos  = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana")
+        );
+
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
